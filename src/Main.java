ef78180e422697d537edb2c6f14263da74df2511
Nyan42 <victor-legros2@hotmail.fr>
2021-12-14 12:20:06 +0100

    test jenkins

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello there");
+        System.out.println("hello there2");
     }
 }
